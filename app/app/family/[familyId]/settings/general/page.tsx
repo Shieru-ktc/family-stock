d4ba6637ac417d492a4a0b97c30cbcfbf75a3623
Shieru-ktc <167487026+Shieru-ktc@users.noreply.github.com>
2025-02-24 12:42:44 +0900

    ファミリー削除を新APIに対応

--- app/app/family/[familyId]/settings/general/page.tsx
+++ app/app/family/[familyId]/settings/general/page.tsx
@@ -5,39 +5,42 @@ import { Member, User } from "@prisma/client";
 import { familyAtom } from "@/atoms/familyAtom";
 import { Button } from "@/components/ui/button";
 import { useAtom } from "jotai";
+import { apiClient } from "@/lib/apiClient";
 
 export default function FamilySettingsPage({
-  params,
+    params,
 }: {
-  params: Promise<{ familyId: string }>;
+    params: Promise<{ familyId: string }>;
 }) {
-  const [family] = useAtom(familyAtom);
+    const [family] = useAtom(familyAtom);
 
-  return family ? (
-    <div>
-      <h1>Family Settings</h1>
-      <p>Family ID: {family.id}</p>
-      <p>Family Name: {family.name}</p>
-      <p>Family Members:</p>
-      <ul>
-        {family.Members.map((member: Member & { User: User }) => (
-          <li key={member.id}>
-            {member.User.name} - {member.User.email}
-          </li>
-        ))}
-      </ul>
-      <Button
-        variant="destructive"
-        onClick={() => {
-          fetch(`/api/family/${family.id}`, {
-            method: "DELETE",
-          });
-        }}
-      >
-        Delete Family
-      </Button>
-    </div>
-  ) : (
-    <p>Family not found.</p>
-  );
+    return family ? (
+        <div>
+            <h1>Family Settings</h1>
+            <p>Family ID: {family.id}</p>
+            <p>Family Name: {family.name}</p>
+            <p>Family Members:</p>
+            <ul>
+                {family.Members.map((member: Member & { User: User }) => (
+                    <li key={member.id}>
+                        {member.User.name} - {member.User.email}
+                    </li>
+                ))}
+            </ul>
+            <Button
+                variant="destructive"
+                onClick={() => {
+                    apiClient.api.family[":familyId"].$delete({
+                        param: {
+                            familyId: family.id,
+                        },
+                    });
+                }}
+            >
+                Delete Family
+            </Button>
+        </div>
+    ) : (
+        <p>Family not found.</p>
+    );
 }
