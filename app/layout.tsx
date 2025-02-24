62bac2dab69876fc08b590fbc81c8a08291c0632
Shieru-ktc <167487026+Shieru-ktc@users.noreply.github.com>
2025-02-25 00:44:28 +0900

    fontをnextから取ってくるように

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,16 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import "./globals.css";
 
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-
-const notoSansJP = localFont({
-    src: "./fonts/NotoSansJP.ttf",
-    variable: "--font-noto-sans-jp",
-});
+import { Noto_Sans_JP } from "next/font/google";
 
 export const metadata: Metadata = {
     title: "Family Stock",
     description: "家庭用の在庫管理アプリ",
 };
+const notoSans = Noto_Sans_JP({
+    subsets: ["latin"],
+    fallback: ["Noto Sans JP"],
+});
 
 export default function RootLayout({
     children,
@@ -22,7 +21,7 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning>
             <body
-                className={`${notoSansJP.variable} antialiased transition-all duration-200`}
+                className={`${notoSans.className} antialiased transition-all duration-200`}
             >
                 <ThemeProvider
                     attribute="class"
