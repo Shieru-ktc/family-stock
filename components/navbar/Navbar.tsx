24da11ecf043f9e4a8aae28b805a1564a50b29e0
Shieru-ktc <167487026+Shieru-ktc@users.noreply.github.com>
2025-02-25 21:24:21 +0900

    ナビゲーションバーの背景色をオレンジに変更し、ダークモードのスタイルを追加

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { SidebarTrigger } from "../ui/sidebar";
 
 export default function NavBar() {
     return (
-        <nav className="bg-slate-700 p-4 px-8 font-light text-white shadow-xl">
+        <nav className="bg-orange-300 p-4 px-8 font-light shadow-xl dark:bg-gray-900 dark:text-white">
             <div className="flex items-center justify-between">
                 <div className="flex items-center space-x-4">
                     <SidebarTrigger className="h-8 w-8" />
