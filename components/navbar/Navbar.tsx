ac37c59f811dfa8dc8e35ca96321dce6273585f2
Shieru-ktc <167487026+Shieru-ktc@users.noreply.github.com>
2025-02-25 21:18:26 +0900

    ナビゲーションバーのタイトルを「App Name」から「Family Stock」に変更

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,14 +1,14 @@
 import { SidebarTrigger } from "../ui/sidebar";
 
 export default function NavBar() {
-  return (
-    <nav className="bg-slate-700 p-4 px-8 font-light text-white shadow-xl">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center space-x-4">
-          <SidebarTrigger className="h-8 w-8" />
-          <div className="text-2xl">App Name</div>
-        </div>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="bg-slate-700 p-4 px-8 font-light text-white shadow-xl">
+            <div className="flex items-center justify-between">
+                <div className="flex items-center space-x-4">
+                    <SidebarTrigger className="h-8 w-8" />
+                    <div className="text-2xl">Family Stock</div>
+                </div>
+            </div>
+        </nav>
+    );
 }
