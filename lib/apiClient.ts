3908aaecddadbc3780f04a931dd923a28572ca7b
Shieru-ktc <167487026+Shieru-ktc@users.noreply.github.com>
2025-03-01 20:55:57 +0900

    HcFetchErrorクラスを追加し、fetchメソッドのエラーハンドリングを改善

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -1,15 +1,27 @@
 import { ApiAppType } from "@/api/src";
 import { hc } from "hono/client";
 
+export class HcFetchError extends Error {
+    constructor(public res: Response) {
+        super(`${res.url} ${res.status} ${res.statusText}`);
+    }
+}
+
 export const apiClient = hc<ApiAppType>(
     process.env.NODE_ENV === "production"
         ? "https://stocks-api.shieru-lab.com"
         : "http://localhost:3030",
     {
-        fetch: (req: string | Request | URL, init: RequestInit | undefined) =>
-            fetch(req, {
+        fetch: async (req: string | Request | URL, init: RequestInit | undefined) => {
+            const res = await fetch(req, {
                 ...init,
                 credentials: "include",
-            }),
+            });
+            if (res.ok) {
+                return res;
+            } else {
+                throw new HcFetchError(res);
+            }
+        }
     },
 );
