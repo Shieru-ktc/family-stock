d8d4098a7688f3995549369dc40e9d8308e59d89
Shieru-ktc <167487026+Shieru-ktc@users.noreply.github.com>
2025-03-01 20:27:55 +0900

    ContactPageコンポーネントをクライアントコンポーネントに変更

--- app/main/contact/page.tsx
+++ app/main/contact/page.tsx
@@ -1,8 +1,10 @@
-export default async function ContactPage() {
-  return (
-    <>
-      <h1 className="text-3xl">お問い合わせ</h1>
-      <p>お問い合わせページです</p>
-    </>
-  );
+"use client";
+
+export default function ContactPage() {
+    return (
+        <>
+            <h1 className="text-3xl">お問い合わせ</h1>
+            <p>お問い合わせページです</p>
+        </>
+    );
 }
