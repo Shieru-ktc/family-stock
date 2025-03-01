69d7c233b498475bc251abac6b66326be21b12ab
Shieru-ktc <167487026+Shieru-ktc@users.noreply.github.com>
2025-03-01 21:21:12 +0900

    fetchメソッドのエラーハンドリングを改善し、エラーメッセージをトースト通知で表示

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -1,18 +1,16 @@
 import { ApiAppType } from "@/api/src";
 import { hc } from "hono/client";
-
-export class HcFetchError extends Error {
-    constructor(public res: Response) {
-        super(`${res.url} ${res.status} ${res.statusText}`);
-    }
-}
+import { toast } from "sonner";
 
 export const apiClient = hc<ApiAppType>(
     process.env.NODE_ENV === "production"
         ? "https://stocks-api.shieru-lab.com"
         : "http://localhost:3030",
     {
-        fetch: async (req: string | Request | URL, init: RequestInit | undefined) => {
+        fetch: async (
+            req: string | Request | URL,
+            init: RequestInit | undefined,
+        ) => {
             const res = await fetch(req, {
                 ...init,
                 credentials: "include",
@@ -20,8 +18,15 @@ export const apiClient = hc<ApiAppType>(
             if (res.ok) {
                 return res;
             } else {
-                throw new HcFetchError(res);
+                const data = await res.clone().json();
+                if ("error" in data) {
+                    toast.error(data.error, {
+                        richColors: true,
+                    });
+                }
+
+                return res;
             }
-        }
+        },
     },
 );
