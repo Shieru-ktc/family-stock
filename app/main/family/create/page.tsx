d095f7d7ae4fa7669752c8fd581f1093fba3be3a
Shieru-ktc <167487026+Shieru-ktc@users.noreply.github.com>
2025-03-02 23:42:57 +0900

    ファミリー作成ページに成功時のリダイレクト機能を追加し、招待リンク処理を実装

--- app/main/family/create/page.tsx
+++ app/main/family/create/page.tsx
@@ -5,36 +5,44 @@ import { useMutation } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { apiClient } from "@/lib/apiClient";
+import { useRouter } from "next/navigation";
 
 export default function FamilyCreatePage() {
-  const createFamily = useMutation({
-    mutationFn: (name: string) => {
-      return apiClient.api.family.$post({
-        json: {
-          name,
-        }
-      })
-    },
-  });
+    const router = useRouter();
+    const createFamily = useMutation({
+        mutationFn: (name: string) => {
+            return apiClient.api.family.$post({
+                json: {
+                    name,
+                },
+            });
+        },
+        onSuccess: async (res) => {
+            if (res.ok) {
+                const family = await res.json();
+                router.push(`/main/family/${family.id}`);
+            }
+        },
+    });
 
-  return (
-    <div>
-      <h1 className="text-2xl">ファミリーを作成</h1>
-      <p>ファミリーを作成すると、家族全員で在庫状況を管理できます。</p>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          const formData = new FormData(e.target as HTMLFormElement);
-          const familyName = formData.get("name") as string;
-          createFamily.mutate(familyName);
-        }}
-      >
-        <label>
-          ファミリー名:
-          <Input type="text" name="name" />
-        </label>
-        <Button type="submit">作成する</Button>
-      </form>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="text-2xl">ファミリーを作成</h1>
+            <p>ファミリーを作成すると、家族全員で在庫状況を管理できます。</p>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    const formData = new FormData(e.target as HTMLFormElement);
+                    const familyName = formData.get("name") as string;
+                    createFamily.mutate(familyName);
+                }}
+            >
+                <label>
+                    ファミリー名:
+                    <Input type="text" name="name" />
+                </label>
+                <Button type="submit">作成する</Button>
+            </form>
+        </div>
+    );
 }
--- app/main/family/join/[inviteId]/page.tsx
+++ app/main/family/join/[inviteId]/page.tsx
@@ -1,4 +1,73 @@
+"use client";
+
+import Loading from "@/components/Loading";
+import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import { apiClient } from "@/lib/apiClient";
+import { useMutation } from "@tanstack/react-query";
+import { InferResponseType } from "hono";
+import { use, useEffect, useState } from "react";
+
 // TODO: 招待リンク(URL)の状態のものを処理するように変更する
-export default async function JoinWithUrl() {
-  return <div>Todo</div>
+export default function JoinWithUrl({
+    params,
+}: {
+    params: Promise<{ inviteId: string }>;
+}) {
+    const inviteId = use(params).inviteId;
+    const [invite, setInvite] = useState<
+        | InferResponseType<
+              (typeof apiClient.api.invite)[":inviteId"]["$get"],
+              200
+          >
+        | undefined
+    >(undefined);
+    const joinFamily = useMutation({
+        mutationFn: async () => {
+            await apiClient.api.invite[":inviteId"].$post({
+                param: {
+                    inviteId,
+                },
+            });
+        },
+    });
+
+    useEffect(() => {
+        apiClient.api.invite[":inviteId"]
+            .$get({
+                param: {
+                    inviteId,
+                },
+            })
+            .then(async (res) => {
+                if (res.ok) {
+                    const invite = await res.json();
+                    setInvite(invite);
+                }
+            });
+    }, [inviteId]);
+
+    return invite ? (
+        <>
+            <h1 className="text-2xl">ファミリーに参加</h1>
+            <Alert className="my-4">
+                <AlertTitle>
+                    ファミリー {invite.Family.name} に招待されています。
+                </AlertTitle>
+                <AlertDescription className="my-4">
+                    <p>
+                        ファミリーに参加するには、以下のボタンをクリックしてください。
+                    </p>
+                    <p className="my-2 rounded bg-yellow-100 p-3 dark:bg-yellow-900">
+                        ファミリーに参加すると、あなたのアカウント名とメールアドレスがメンバーに共有されます。
+                    </p>
+                    <Button onClick={() => joinFamily.mutate()}>
+                        参加する
+                    </Button>
+                </AlertDescription>
+            </Alert>
+        </>
+    ) : (
+        <Loading />
+    );
 }
